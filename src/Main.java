7faeb3c999d929f2d527a0911f5513516953b64e
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-06-25 21:49:37 +0300

    Q5

--- src/Main.java
+++ src/Main.java
@@ -66,5 +66,27 @@ public class Main {
 
         System.out.println(exponentResult);
 
+//        5.Write a program that reads a set of integers, and then prints the sum of
+//        the even and odd integers.
+
+        System.out.println("sum of odd numbers and sum of even numbers");
+
+        int evenNumbersSum = 0;
+        int oddNumbersSum = 0;
+
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+        System.out.println("for the following set:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]+" ");
+            if (numbers[i]%2 == 0){
+                evenNumbersSum+=numbers[i];
+            } else {
+                oddNumbersSum+=numbers[i];
+            }
+        }
+        System.out.println();
+        System.out.println("sum of even numbers = "+evenNumbersSum);
+        System.out.println("sum of odd numbers = "+oddNumbersSum);
     }
 }
