75acf97d8cafbd8ebdab744e22e158645563ab9e
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-06-25 20:59:26 +0300

    Q2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
 //        1.Write a program that prints the numbers from 1 to 100 such that:
 //        If the number is a multiple of 3, you need to print "Fizz" instead of that
 //        number.
-
+        System.out.println("Printing numbers game:");
         for (int i = 1; i<=100; i++){
             if(i%3 == 0 && i%5 == 0){
                 System.out.println("FizzBuzz");
@@ -19,5 +21,15 @@ public class Main {
 
             System.out.println(i);
         }
+
+//        2.Write a Java program to reverse a string.
+//        Test Data: Input a string: The quick brown fox Expected Output: Reverse
+//        string: xof nworb kciuq ehT
+        System.out.println("enter an entire sentence to reverse: ");
+        Scanner input = new Scanner(System.in);
+        StringBuilder sentence = new StringBuilder(input.nextLine());
+
+        System.out.println(sentence.reverse());
+
     }
 }
